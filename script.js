a013aa64f8e3baf84c89c28030ac591fa5a0c3fa
Akash M <108210779+akashikur@users.noreply.github.com>
2023-06-26 17:56:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //your code here
+let touristSports=['The Virupaksha Temple','Victoria Menmorial','Tajmahal'];
+let articals=['a','an','the'];
 
+
+
+
+
+const brand=document.getElementById("brand");
+brand.innerHtml=`<li>Viru</li>`
